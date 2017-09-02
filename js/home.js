2ae169b72f085f319933ded3cdb82be014c93b8c
Michael Ma <mma01us@gmail.com>
2017-09-02 17:50:37 -0400

    try 3

--- js/home.js
+++ js/home.js
@@ -1,9 +1,13 @@
 // JavaScript Document
 
-$('textarea').each(function () {
-  this.setAttribute('style', 'height:' + (this.scrollHeight) + 'px;overflow-y:hidden;');
-}).on('input', function () {
+var tx = document.getElementsByTagName('textarea');
+for (var i = 0; i < tx.length; i++) {
+  tx[i].setAttribute('style', 'height:' + (tx[i].scrollHeight) + 'px;overflow-y:hidden;');
+  tx[i].addEventListener("input", OnInput, false);
+}
+
+function OnInput() {
   this.style.height = 'auto';
   this.style.height = (this.scrollHeight) + 'px';
-});
+}
 
