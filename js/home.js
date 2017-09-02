7d7b661f95378b05ef6c860ed6854606194648db
Michael Ma <mma01us@gmail.com>
2017-09-02 17:43:27 -0400

    Added autoresize v1

--- js/home.js
+++ js/home.js
@@ -1,2 +1,9 @@
 // JavaScript Document
 
+$('textarea').each(function () {
+  this.setAttribute('style', 'height:' + (this.scrollHeight) + 'px;overflow-y:hidden;');
+}).on('input', function () {
+  this.style.height = '90px';
+  this.style.height = (this.scrollHeight) + 'px';
+});
+
