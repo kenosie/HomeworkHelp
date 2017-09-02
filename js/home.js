a4b18855c8657715fd56e16bd92a98a25dc1f772
Michael Ma <mma01us@gmail.com>
2017-09-02 17:45:54 -0400

    autoresize test2

--- js/home.js
+++ js/home.js
@@ -3,7 +3,7 @@
 $('textarea').each(function () {
   this.setAttribute('style', 'height:' + (this.scrollHeight) + 'px;overflow-y:hidden;');
 }).on('input', function () {
-  this.style.height = '90px';
+  this.style.height = 'auto';
   this.style.height = (this.scrollHeight) + 'px';
 });
 
